135420cdd01b5d3c2b1c0a4a22ea89ad3d0412db
Yeonsu1027 <rito1205@naver.com>
2023-12-14 15:49:53 +0900

    black restart 추가완성

--- bbbbbllackjack/src/Service/BlackJackCardGameNoreStartVer.java
+++ bbbbbllackjack/src/Service/BlackJackCardGameNoreStartVer.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 
 import utils.Line;
 
-public class BlackJackCardGame {
+public class BlackJackCardGameNoreStartVer {
 	//카드 숫자의 합이 21을 초과하게 되는 순간 '버스트'라고 하며 
 			//딜러의 결과에 관계없이 플레이어가 패배한다. -나무위키
 	
